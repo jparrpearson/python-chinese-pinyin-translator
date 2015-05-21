5e4c3a11a977c4e64b79c3c5e5003b0f9351b9cd
Jeremy Parr-Pearson <jparrpearson@users.noreply.github.com>
2015-05-20 20:58:18 -0700

    Added process timing and options information

--- translate.py
+++ translate.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import sys
-import io
-import getopt
 import codecs
-import re
+import getopt
+import io
 import os
+import re
 import shutil
+import sys
+import time
 
-# TODO: Add timing information (dictionary load and for each file translation)
 # TODO: Implement filename support (individual or in directory)
-# TODO: Add information on parameters (part of usage)
 
 def main(argv):
     dictionaryFile = "resources/cedict_ts.u8"
@@ -25,7 +24,13 @@ def main(argv):
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding=sys.stdout.encoding, errors="replace")
 
     # Get program arguments
-    usage = "translate.py -h -f <file> -d <dir> -t <tones> -c <capitalize> -b <backup>"
+    usage = "Example usage:\n  translate.py -h -f <file> -d <dir> -t <tones> -c <capitalize> -b <backup>"
+    options = ("Options:\n  -h, --help - Shows the script usage and help options.\n"
+        "  -f, --file - The input file to translate.  One of 'file' or 'dir' is required.\n"
+        "  -d, --dir - The input directory to translate (translates all files in nested directories).  One of 'dir' or 'file' is required.\n"
+        "  -t, --tones - Output the pinyin tone numbers.  Defaults to 'false'.\n"
+        "  -c, --capitalize - Capitalize the pinyin (otherwise all lower case).  Defaults to 'true'.\n"
+        "  -b, --backup - Backup each translated file ('filename.ext.BAK').  Defaults to 'true'.\n")
     try:
         opts, args = getopt.getopt(argv, "hf:d:t:c:b:", ["help", "file=", "dir=", "tones=", "capitalize=", "backup="])
     except getopt.GetoptError as err:
@@ -35,6 +40,8 @@ def main(argv):
     for opt, arg in opts:
         if opt in ("-h", "--help"):
             print(usage)
+            print(options)
+            sys.exit(0)
         elif opt in ("-f", "--file"):
             inputFile = arg
         elif opt in ("-d", "--dir"):
@@ -46,7 +53,13 @@ def main(argv):
         elif opt in ("-b", "--backup"):
             backup = True if arg.upper() == "TRUE" else False
 
+    if not inputFile and not inputDir:
+        print("Must provide either a file or directory to translate")
+        print(usage)
+        sys.exit(2)
+
     # Parse the dictionary file into a local dictionary
+    start = time.time()
     global dict
     dict = {}
     with codecs.open(dictionaryFile, "r", encoding="utf-8") as file:
@@ -61,16 +74,13 @@ def main(argv):
                     # Add keys for traditional and simplified characters
                     dict[match.group(1)] = pinyin
                     dict[match.group(2)] = pinyin
-    print("Loaded " + str(len(dict)) + " dictionary entries")
+    print("Loaded " + str(len(dict)) + " dictionary entries (" + getTime(start) + "s)")
 
     # Perform translation actions
     if inputFile:
         translateFile(inputFile, backup)
     elif inputDir:
         translateDir(inputDir, backup)
-    else:
-        print("Must provide either a file or directory to translate")
-        print(usage)
 
 def translateFile(inputFile, backup):
     print("Translating file " + inputFile + "...")
@@ -113,6 +123,12 @@ def translateDir(inputDir, backup):
             if not filename.endswith(".BAK"):
                 translateFile(filename, backup)
 
+def getTime(start, digits=2):
+    end = time.time()
+    f = "{:." + str(digits) + "f}"
+    return f.format(end-start)
+
 if __name__ == "__main__":
+    start = time.time()
     main(sys.argv[1:])
-    print("Done")
+    print("Done (" + getTime(start) +"s)")
